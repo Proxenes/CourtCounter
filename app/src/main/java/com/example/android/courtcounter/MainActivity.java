d16d7bb266cdfa0e1e3553a00851ff78df2dfc16
proxenes <skyler.tisue@gmail.com>
2019-02-14 23:08:18 -0600

    Team A LinearLayout completely functional.

--- app/src/main/java/com/example/android/courtcounter/MainActivity.java
+++ app/src/main/java/com/example/android/courtcounter/MainActivity.java
@@ -1,13 +1,61 @@
 package com.example.android.courtcounter;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    // Variable used to store the total score for Team A
+    int teamAScore;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Test functionality of below method(s)
+        // displayForTeamA(8);
+
+        // Set the value of teamAScore to the text value of the
+        // team_a_score TextView
+        TextView teamAScoreTextView = findViewById(R.id.team_a_score);
+        teamAScore = Integer.parseInt(teamAScoreTextView.getText().toString());
+    }
+
+    /**
+     * Displays the total score for Team A in the team_a_score TextView
+     */
+    public void displayForTeamA(int score) {
+        TextView priceTextView=(findViewById(R.id.team_a_score));
+        priceTextView.setText(String.valueOf(score));
+    }
+
+    /**
+     * Increases teamAScore by 3 then updates the team_a_score TextView
+     * by calling the displayForTeamA method
+     */
+    public void teamAThreePoints(View view) {
+        teamAScore += 3;
+        displayForTeamA(teamAScore);
+    }
+
+    /**
+     * Increases teamAScore by 2 then updates the team_a_score TextView
+     * by calling the displayForTeamA method
+     */
+    public void teamATwoPoints(View view) {
+        teamAScore += 2;
+        displayForTeamA(teamAScore);
+    }
+
+    /**
+     * Increases teamAScore by 1 then updates the team_a_score TextView
+     * by calling the displayForTeamA method
+     */
+    public void teamAFreeThrow(View view) {
+        teamAScore += 1;
+        displayForTeamA(teamAScore);
     }
 }
