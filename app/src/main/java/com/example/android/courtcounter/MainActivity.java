82e449b91f5dcac3b92112146d6629e5c168b772
proxenes <skyler.tisue@gmail.com>
2019-02-17 02:12:16 -0600

    Working on formatting.

--- app/src/main/java/com/example/android/courtcounter/MainActivity.java
+++ app/src/main/java/com/example/android/courtcounter/MainActivity.java
@@ -120,4 +120,14 @@ public class MainActivity extends AppCompatActivity {
         teamBScore += 1;
         displayForTeamB(teamBScore);
     }
+
+    /**
+     * Resets scores for both teams to 0
+     */
+    public void resetScores(View view) {
+        teamAScore = 0;
+        teamBScore = 0;
+        displayForTeamA(teamAScore);
+        displayForTeamB(teamBScore);
+    }
 }
